2ecba3dd5d453b457ac86b7fa64fe9e68344daf3
Ahmed_Mahmoud <ahmedmahmuuud@gmail.com>
2026-08-06 23:34:14 +0400

    chore: configure application foundation

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+
 import { App } from './app';
 
 describe('App', () => {
-  beforeEach(async () => {
+  it('creates the application', async () => {
     await TestBed.configureTestingModule({
       imports: [App],
     }).compileComponents();
-  });
 
-  it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pleny-product-explorer');
+    expect(fixture.componentInstance).toBeTruthy();
   });
 });
